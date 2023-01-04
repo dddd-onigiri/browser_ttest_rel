f8e725ae141f2af7efd5dd8d4c0fe813a3c97087
dddd-onigiri <dddd.sailing470@gmail.com>
2023-01-04 22:56:40 +0900

    修正

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+
 import streamlit as st
 import pandas as pd
 # 実装予定
@@ -115,85 +117,49 @@ with st.form(key='check_form'):
 with st.form(key='analyze_form'):
     if TTEST_btn:
         st.subheader('【分析結果】')
-        st.write('【要約統計量】')
 
         # 各値の初期化
         n = 1
         m = 0
         # リストの名前を取得
-        summaryList = []
+        VariableList = []
         for ListAppend in range(ovRange):
-            summaryList.append(
+            VariableList.append(
                 f'【'f'{(ObservedVariable[m])}'f'】　→　【'f'{(MeasuredVariable[m])}】')
             m += 1
 
-        summaryColumns = ["有効N", "平均値", "中央値", "標準偏差", "分散",
-                          "最小値", "最大値"]
-
         # 観測値、測定値から作業用データフレームのセット
-        df00_list = [ObservedVariable]
-        df00_list = df00_list + MeasuredVariable
-        df00 = df[df00_list]
+        # df00_list = [ovList]
+        # df00_list = df00_list + mvList
+        dfOv = df[ObservedVariable]
+        dfMv = df[MeasuredVariable]
 
-        st.write(df00)
-
-        # サマリ(df0)用のデータフレームのセット
-        df0 = pd.DataFrame(index=summaryList, columns=summaryColumns)
-
-        # サマリ(df0)用のデータフレームに平均値と標準偏差を追加
-        for summary in range(ovRange):
-            # 列データの取得（nは従属変数の配列番号）
-            y = df00.iloc[:, n]
-
-            # 従属変数の列データの計算処理
-            df0.at[df00.columns[n], '有効N'] = len(y)
-            df0.at[df00.columns[n], '平均値'] = y.mean()
-            df0.at[df00.columns[n], '中央値'] = median(y)
-            df0.at[df00.columns[n], '標準偏差'] = y.std()
-            df0.at[df00.columns[n], '分散'] = variance(y)
-            df0.at[df00.columns[n], '最小値'] = y.min()
-            df0.at[df00.columns[n], '最大値'] = y.max()
-            n += 1
-
-        # 要約統計量（サマリ）のデータフレームを表示
-        st.dataframe(df0)
-
-        st.write('【平均値の差の検定】')
+        st.write('【平均値の差の検定（対応あり）】')
 
         # 各値の初期化
-        n = 1
-
-        st.write(summaryList)
-
-'''
+        n = 0
         # t検定結果用データフレーム（df1）の列を指定
-        summaryColumns = ['全体M', '全体S.D', DivideVariable[0] + "M",
-                          DivideVariable[0] + "S.D", DivideVariable[1] + "M",
-                          DivideVariable[1] + "S.D", 'df', 't', 'p', 'sign',
-                          'd']
-        df1 = pd.DataFrame(index=summaryList, columns=summaryColumns)
+        summaryColumns = ["観測値" + "M", "観測値" + "S.D",
+                          "測定値" + "M", "測定値" + "S.D",
+                          'df', 't', 'p', 'sign', 'd']
 
-        for summary in range(dvRange):
-            # 列データの取得（nは従属変数の配列番号）
-            y = df00.iloc[:, n]
+        df1 = pd.DataFrame(index=VariableList, columns=summaryColumns)
 
-            # df（元データ）男性でフィルターしたデータフレームをセット
-            dv0 = df00[df00[IndependentVariable] == DivideVariable[0]]
-            dv1 = df00[df00[IndependentVariable] == DivideVariable[1]]
-
-            # フィルターした列データの取得（nは従属変数の配列番号）
-            dv0y = dv0.iloc[:, n]
-            dv1y = dv1.iloc[:, n]
+        for ttest_rel in range(ovRange):
+            # 列データの取得（nは変数の配列番号）
+            x = dfOv.iloc[:, n]
+            y = dfMv.iloc[:, n]
 
             # t値、p値、s（全体標準偏差）、d値（効果量）の取得
-            ttest = stats.ttest_ind(dv0y, dv1y, equal_var=False)
+            ttest = stats.ttest_rel(x, y)
             t = abs(ttest[0])
             p = ttest[1]
-            s = y.std()
-            dv0ym = dv0y.mean()
-            dv1ym = dv1y.mean()
-            d_beta = dv0ym - dv1ym
-            d = abs(d_beta) / s
+            xs = x.std()
+            ys = y.std()
+            xm = x.mean()
+            ym = y.mean()
+            # d_beta = xm - ym
+            # d = abs(d_beta/math.sqrt((len(x)-1)))
 
             # p値の判定をsignに格納
             sign = ""
@@ -207,78 +173,29 @@ with st.form(key='analyze_form'):
                 sign = "n.s."
 
             # 従属変数の列データの計算処理
-            df1.at[df00.columns[n], '全体M'] = y.mean()
-            df1.at[df00.columns[n], '全体S.D'] = y.std()
-            df1.at[df00.columns[n], DivideVariable[0] + "M"] = dv0y.mean()
-            df1.at[df00.columns[n], DivideVariable[0] + "S.D"] = dv0y.std()
-            df1.at[df00.columns[n], DivideVariable[1] + "M"] = dv1y.mean()
-            df1.at[df00.columns[n], DivideVariable[1] + "S.D"] = dv1y.std()
-            df1.at[df00.columns[n], 'df'] = len(y) - 1
-            df1.at[df00.columns[n], 't'] = t
-            df1.at[df00.columns[n], 'p'] = p
-            df1.at[df00.columns[n], 'sign'] = sign
-            df1.at[df00.columns[n], 'd'] = d
+            df1.at[VariableList[n], '観測値M'] = xm
+            df1.at[VariableList[n], '観測値S.D'] = xs
+            df1.at[VariableList[n], '測定値M'] = ym
+            df1.at[VariableList[n], '測定値S.D'] = ys
+            df1.at[VariableList[n], 'df'] = len(x) - 1
+            df1.at[VariableList[n], 't'] = t
+            df1.at[VariableList[n], 'p'] = p
+            df1.at[VariableList[n], 'sign'] = sign
+            df1.at[VariableList[n], 'd'] = "アプデ"
 
             n += 1
 
         st.dataframe(df1)
 
         # サンプルサイズの取得
-        sample_n = len(df00)
-        sample_0 = len(dv0)
-        sample_1 = len(dv1)
+        sample_n = len(dfOv)
 
         st.write('【サンプルサイズ】')
         st.write(f'全体N ＝'f'{sample_n}')
-        st.write(f'● {DivideVariable[0]}：'f'{sample_0}')
-        st.write(f'● {DivideVariable[1]}：'f'{sample_1}')
 
         st.write('【分析結果の解釈】')
-
-        # 各値の初期化、簡素化
-        n = 0
-        d0 = DivideVariable[0]
-        d1 = DivideVariable[1]
-        iv = IndependentVariable
-
-        # sign の列番号を取得
-        sign_n = df1.columns.get_loc('sign')
-        # DivideVariable[0] + 'M' の列番号を取得
-        d0n = df1.columns.get_loc(d0 + "M")
-        # DivideVariable[1] + 'M' の列番号を取得
-        d1n = df1.columns.get_loc(d1 + "M")
-
-        for interpretation in range(dvRange):
-            dn = DependentVariable[n]
-            if df1.iat[n, sign_n] == "**":
-                if df1.iat[n, d0n] > df1.iat[n, d1n]:
-                    st.write(
-                        f'{iv}によって【'f'{dn}】には有位な差が生まれる'f'（{d0}＞'f'{d1}）')
-                elif df1.iat[n, d0n] < df1.iat[n, d1n]:
-                    st.write(
-                        f'{iv}によって【'f'{dn}】には有意な差が生まれる'f'（{d1}＞'f'{d0}）')
-            elif df1.iat[n, sign_n] == "*":
-                if df1.iat[n, d0n] > df1.iat[n, d1n]:
-                    st.write(
-                        f'{iv}によって【'f'{dn}】には有意な差が生まれる'f'（{d0}＞'f'{d1}）')
-                elif df1.iat[n, d0n] < df1.iat[n, d1n]:
-                    st.write(
-                        f'{iv}によって【'f'{dn}】には有意な差が生まれる'f'（{d1}＞'f'{d0}）')
-            elif df1.iat[n, sign_n] == "†":
-                if df1.iat[n, d0n] > df1.iat[n, d1n]:
-                    st.write(
-                        f'{iv}によって【'f'{dn}】には有意な差が生まれる傾向にある'f'（{d0}＞'f'{d1}）')
-                elif df1.iat[n, d0n] < df1.iat[n, d1n]:
-                    st.write(
-                        f'{iv}によって【'f'{dn}】には有意な差が生まれる傾向にある'f'（{d1}＞'f'{d0}）')
-            elif df1.iat[n, sign_n] == "n.s.":
-                st.write(f'{iv}によって【'f'{dn}】には有意な差が生まれない')
-
-            n += 1
 
         TTEST_btn = st.form_submit_button('OK')
 
 st.write('ご意見・ご要望は→', 'https://forms.gle/G5sMYm7dNpz2FQtU9', 'まで')
 st.write('© 2022 Daiki Ito. All Rights Reserved.')
-
-'''
